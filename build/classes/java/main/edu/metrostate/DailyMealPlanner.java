ccc6443c1d943032e1974b9f54f234dea1cca1c5
hpham24 <hieu.pham@my.metrostate.edu>
2024-09-24 19:35:07 -0500

    Create DailyMealPlanner.java

---
+++ build/classes/java/main/edu/metrostate/DailyMealPlanner.java
@@ -0,0 +1,11 @@
+import java.sql.Date;
+
+public class DailyMealPlanner {
+	private String day;
+	private Date date;
+	private Recipe breakfast;
+	private Recipe lunch;
+	private Recipe dinner;
+	private Recipe snack;
+	private Recipe drink;
+}
