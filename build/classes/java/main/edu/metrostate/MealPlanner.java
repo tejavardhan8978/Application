7f59545125c5bd2186c2892bcef648475869ed8b
hpham24 <hieu.pham@my.metrostate.edu>
2024-09-26 10:16:41 -0500

    Update MealPlanner.java
    updated to include getters and setters

--- build/classes/java/main/edu/metrostate/MealPlanner.java
+++ build/classes/java/main/edu/metrostate/MealPlanner.java
@@ -1,11 +1,36 @@
 import java.util.Queue;
 
 public class MealPlanner {
-    public MealPlanner(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	private int id;
+    private int id;
     private String name;
-    private Queue<DailyMealPlanner)>;
+    private Queue<DailyMealPlanner> dailyMeals;
+	
+    public MealPlanner(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+	
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Queue<DailyMealPlanner> getDailyMeals() {
+        return dailyMeals;
+    }
+
+    public void setDailyMeals(Queue<DailyMealPlanner> dailyMeals) {
+        this.dailyMeals = dailyMeals;
+    }
 }
