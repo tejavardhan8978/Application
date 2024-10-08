912fc470f4615ab3f14a3b644de4d4e972c0a263
Hunterbounty11 <hunterbamm@gmail.com>
2024-10-08 11:32:11 -0500

    Changed Ingredients and Categories

--- src/main/java/edu/metrostate/Model/Categories/Ingredient.java
+++ src/main/java/edu/metrostate/Model/Categories/Ingredient.java
@@ -8,7 +8,7 @@ import edu.metrostate.Model.Storage;
 import java.util.ArrayList;
 import java.util.Date;
 
-public abstract class Ingredient {
+public class Ingredient {
 
     private int id;
     private String name;
@@ -16,16 +16,29 @@ public abstract class Ingredient {
     private NutritionalChart nutrition;
     private MacroNutrient primaryMacroNutrient;
     private Storage storage;
-
-    public Ingredient(int id, String name, Date expiryDate, NutritionalChart nutrition, MacroNutrient primaryMacroNutrient, Storage storage) {
+    private int quantity;
+    private String category;
+    //Buying an ingredient for the first time
+    public boolean CreateNewIngredient(int id, String name, Date expiryDate, NutritionalChart nutrition, MacroNutrient primaryMacroNutrient, Storage storage, int quantity, String category){
         this.id = id;
         this.name = name;
         this.expiryDate = expiryDate;
         this.nutrition = nutrition;
         this.primaryMacroNutrient = primaryMacroNutrient;
         this.storage = storage;
+        this.quantity = quantity;
+        this.category = category;
+        return true;
     }
-
+    //This will be the method to update quantity as more ingredients are bought
+    public boolean UpdateQuantity(int id, int NewQuantity){
+        this.id = id;
+        this.quantity = NewQuantity;
+    }
+    //This Will Search through all stored ingredients to find matching ID and return quantity of it
+    public int GetQuantity(int id){
+        return quantity;
+    }
     /**
      * This method takes an ingredient as a parameter and returns a list of recipes that use this specific ingredient.
      * @param ingredient Ingredient to be searched for.
