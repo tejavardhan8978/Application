360aedfc9c2362c621ec117ff65997f2231f3498
Hunterbounty11 <hunterbamm@gmail.com>
2024-10-22 03:15:52 -0500

    Finished the edit inventory quantities
    Still a bug that the table dissapears but it reappears when you enter and cancel a new ingredient

--- src/main/java/edu/metrostate/Controller/InventoryController.java
+++ src/main/java/edu/metrostate/Controller/InventoryController.java
@@ -13,14 +13,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
+
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class InventoryController implements Initializable {
@@ -29,14 +35,22 @@ public class InventoryController implements Initializable {
     private Parent root;
     private IngredientList ingredientList;
 
-    @FXML private TableView<Ingredient> inventoryTable;
-    @FXML private TableColumn<Ingredient, Integer> idColumn;
-    @FXML private TableColumn<Ingredient, String> name;
-    @FXML private TableColumn<Ingredient, Date> expiryDate;
-    @FXML private TableColumn<Ingredient, MacroNutrient> primaryMacroNutrient;
-    @FXML private TableColumn<Ingredient, Storage> storage;
-    @FXML private TableColumn<Ingredient, Integer> quantity;
-    @FXML private TableColumn<Ingredient, String> category;
+    @FXML
+    private TableView<Ingredient> inventoryTable;
+    @FXML
+    private TableColumn<Ingredient, Integer> idColumn;
+    @FXML
+    private TableColumn<Ingredient, String> name;
+    @FXML
+    private TableColumn<Ingredient, Date> expiryDate;
+    @FXML
+    private TableColumn<Ingredient, MacroNutrient> primaryMacroNutrient;
+    @FXML
+    private TableColumn<Ingredient, Storage> storage;
+    @FXML
+    private TableColumn<Ingredient, Integer> quantity;
+    @FXML
+    private TableColumn<Ingredient, String> category;
 
     public void switchToHome(MouseEvent event) throws IOException {
         root = FXMLLoader.load(Main.class.getResource("/HomeScreen.fxml"));
@@ -60,23 +74,10 @@ public class InventoryController implements Initializable {
         stage.show();
     }
 
-    public void switchToEditIngredient(MouseEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/ChangeQuantityInventoryModal.fxml"));
-        Parent root = loader.load();
-        // Get the controller
-        EditIngredient controller = loader.getController();
-        // Switch to the new scene
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
-//        ingredientList = IngredientListSingleton.getInstance();
-//        updateTableView();
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ingredientList = IngredientListSingleton.getInstance();
+        updateTableView();
         idColumn.setCellValueFactory(new PropertyValueFactory<Ingredient, Integer>("id"));
         name.setCellValueFactory(new PropertyValueFactory<Ingredient, String>("name"));
         expiryDate.setCellValueFactory(new PropertyValueFactory<Ingredient, Date>("expiryDate"));
@@ -85,15 +86,17 @@ public class InventoryController implements Initializable {
         quantity.setCellValueFactory(new PropertyValueFactory<Ingredient, Integer>("quantity"));
         category.setCellValueFactory(new PropertyValueFactory<Ingredient, String>("category"));
 
+
         inventoryTable.setItems(getIngredientItems());
+        inventoryTable.refresh();
     }
 
-    public ObservableList<Ingredient> getIngredientItems(){
+    public ObservableList<Ingredient> getIngredientItems() {
         ObservableList<Ingredient> ingredientList = FXCollections.observableArrayList();
-        ingredientList.add(new Ingredient(1, "Chicken", new java.util.Date(), new NutritionalChart(), MacroNutrient.PROTEIN, Storage.FRIDGE, 5, "Meat", "good meat"));
-        ingredientList.add(new Ingredient(2, "Mutton", new java.util.Date(), new NutritionalChart(), MacroNutrient.PROTEIN, Storage.FRIDGE, 5, "Meat", "best meat"));
-        ingredientList.add(new Ingredient(3, "Broccoli", new java.util.Date(), new NutritionalChart(), MacroNutrient.FIBER, Storage.FRIDGE, 5, "Vegetable", "Least favoured vegetable"));
-        ingredientList.add(new Ingredient(4, "Beans", new java.util.Date(), new NutritionalChart(), MacroNutrient.FIBER, Storage.FRIDGE, 5, "Vegetable", "no comments"));
+//        ingredientList.add(new Ingredient(1, "Chicken", new java.util.Date(), new NutritionalChart(), MacroNutrient.PROTEIN, Storage.FRIDGE, 5, "Meat", "good meat"));
+//        ingredientList.add(new Ingredient(2, "Mutton", new java.util.Date(), new NutritionalChart(), MacroNutrient.PROTEIN, Storage.FRIDGE, 5, "Meat", "best meat"));
+//        ingredientList.add(new Ingredient(3, "Broccoli", new java.util.Date(), new NutritionalChart(), MacroNutrient.FIBER, Storage.FRIDGE, 5, "Vegetable", "Least favoured vegetable"));
+//        ingredientList.add(new Ingredient(4, "Beans", new java.util.Date(), new NutritionalChart(), MacroNutrient.FIBER, Storage.FRIDGE, 5, "Vegetable", "no comments"));
 
         return ingredientList;
 
@@ -145,5 +148,21 @@ public class InventoryController implements Initializable {
     }
 
 
+    public void switchToEditIngredient(MouseEvent event) throws IOException {
+        if (inventoryTable.getSelectionModel().getSelectedItem() != null) {
+            tempIngredient = inventoryTable.getSelectionModel().getSelectedItem();
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/ChangeQuantityInventoryModal.fxml"));
+            Parent root = loader.load();
+            // Get the controller
+            EditIngredient controller = loader.getController();
+            // Switch to the new scene
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
 
+
+    public static Ingredient tempIngredient;
 }
--- src/main/java/edu/metrostate/Model/Ingredient.java
+++ src/main/java/edu/metrostate/Model/Ingredient.java
@@ -29,9 +29,10 @@ public class Ingredient {
     }
     public Ingredient(){}
     //This will be the method to update quantity as more ingredients are bought
-    public boolean UpdateQuantity(int id, int NewQuantity){
+    public boolean UpdateIngredient(int id, int NewQuantity, Date NewDate){
         this.id = id;
         this.quantity = NewQuantity;
+        this.expiryDate = NewDate;
         return false;
     }
     //This Will Search through all stored ingredients to find matching ID and return quantity of it
