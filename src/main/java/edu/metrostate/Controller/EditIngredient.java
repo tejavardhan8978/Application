8a452c8c2ac9abb4cd7764a878b1e4427eed9c20
Hunterbounty11 <hunterbamm@gmail.com>
2024-11-12 14:13:49 -0600

    Edit Ingredient
    Edit ingredient should work with database. All that is left is finding out how to switch to it with fxml

--- src/main/java/edu/metrostate/Controller/EditIngredient.java
+++ src/main/java/edu/metrostate/Controller/EditIngredient.java
@@ -2,6 +2,8 @@ package edu.metrostate.Controller;
 
 import edu.metrostate.Main;
 import edu.metrostate.Model.Ingredient;
+import edu.metrostate.Model.IngredientList;
+import edu.metrostate.Model.IngredientListSingleton;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -10,11 +12,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.*;
 import java.io.IOException;
-import java.sql.Date;
 import java.time.LocalDate;
 import java.util.Objects;
+
+
 
 public class EditIngredient {
     @FXML
@@ -31,8 +37,8 @@ public class EditIngredient {
         stage.setScene(scene);
         stage.show();
     }
-
     public void saveButton(MouseEvent event) throws IOException {
+
         Ingredient tempIngredient = InventoryController.tempIngredient;
         String quantityString = addedQuantity.getText();
         int quantity = Integer.parseInt(quantityString);
@@ -43,10 +49,33 @@ public class EditIngredient {
         LocalDate TempDate = LocalDate.parse(DateString);
         Date updateDate = java.sql.Date.valueOf(TempDate);
 
+
+        //IngredientList ingredientList = IngredientListSingleton.getInstance();
+        //ingredientList.updateIngredient(tempIngredient, tempQuantity, updateDate);
+
         root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("/Views/Inventory-Home.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    public static void UpdateData(int updateID, int updateQuantity, Date newExpiry){
+        String query ="UPDATE IngredientTable SET expiryDate = ?, quantity = ? WHERE id = ?";
+        try (Connection conn= DatabaseConnection.connect();
+             PreparedStatement preparedStatement = conn.prepareStatement(query)){
+            preparedStatement.setDate(1, newExpiry);
+            preparedStatement.setInt(2, updateQuantity);
+            preparedStatement.setInt(3, updateID);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            System.out.println("DID THE UPDATE");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+
+
+
+
     }
 }
