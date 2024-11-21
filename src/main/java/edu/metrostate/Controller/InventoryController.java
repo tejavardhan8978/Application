cc659a0b86c8443ae0e9052d4182aef58bd9242f
tejavardhanv <109634526+tejavardhanv@users.noreply.github.com>
2024-11-21 01:27:48 -0600

    Recipe migrated to DB
    Most of the recipe functionality has been migrated to work on SQLite DB.

--- src/main/java/edu/metrostate/Controller/InventoryController.java
+++ src/main/java/edu/metrostate/Controller/InventoryController.java
@@ -31,7 +31,6 @@ public class InventoryController implements Initializable {
     Scene scene;
     Parent root;
     IngredientList ingredientList;
-    private boolean initialInventoryFlag = false;
     public static Ingredient tempIngredient;
 
     @FXML private TableView<Ingredient> inventoryTable;
@@ -93,7 +92,7 @@ public class InventoryController implements Initializable {
             FXMLLoader loader = new FXMLLoader(Main.class.getResource("/Views/ChangeQuantityInventoryModal.fxml"));
             Parent root = loader.load();
             // Get the controller
-            EditIngredient controller = loader.getController();
+            //EditIngredient controller = loader.getController();
             // Switch to the new scene
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Scene scene = new Scene(root);
@@ -104,7 +103,6 @@ public class InventoryController implements Initializable {
 
     public void updateTableView() {
         System.out.println("updateTableView - inventoryController");
-        System.out.println("Ingredients loaded: " + ingredientList.getIngredients().size());
         ObservableList<Ingredient> items = FXCollections.observableArrayList(ingredientList.getIngredients());
         inventoryTable.setItems(items);
         inventoryTable.refresh();
@@ -114,26 +112,20 @@ public class InventoryController implements Initializable {
         if (event.getClickCount() == 2) {
             Ingredient tempIngredient = inventoryTable.getSelectionModel().getSelectedItem();
             if (tempIngredient != null) {
-                System.out.println(tempIngredient);
-
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/IngredientDetailedModal.fxml"));
-
                 AnchorPane ingredientModal = fxmlLoader.load();
                 IngredientPopupController ingredientPopupController = fxmlLoader.getController();
                 ingredientPopupController.setInventoryController(this);
                 ingredientPopupController.setIngredientModalDetails(tempIngredient);
-
 
                 stage = new Stage();
                 scene = new Scene(ingredientModal);
                 stage.setScene(scene);
                 stage.setTitle("Ingredient Popup");
                 stage.initModality(Modality.APPLICATION_MODAL);
-                System.out.println(this);
 
                 stage.show();
                 ingredientPopupController.setIngredientPopupStage(stage);
-
             }
         }
     }
@@ -143,7 +135,6 @@ public class InventoryController implements Initializable {
     }
 
     public void backToHome(MouseEvent event) throws IOException {
-        System.out.println("Going back to home");
         switchToHome(event);
     }
 
@@ -221,17 +212,3 @@ public class InventoryController implements Initializable {
         performIngredientSearch(searchTerm);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
